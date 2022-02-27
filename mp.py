aff205d3ae1d09558d6cb78b0b39a764d28dae05
anatbenmoshe <62757587+anatbenmoshe@users.noreply.github.com>
2022-02-27 10:06:01 +0200

    Initial commit

---
+++ mp.py
@@ -0,0 +1,14 @@
+
+
+print(abs(3.0 * (4.0 / 3.0 - 1) - 1))  # print inaccurate result
+
+
+def machineEpsilon():  # find and return machine epsilon
+    epsilon = 0.5
+    while (1 + epsilon) != 1:  # while it is accurate
+        epsilon = epsilon / 2  # find smaller epsilon
+    return epsilon  # return machine epsilon
+
+
+print(machineEpsilon())  # print machine epsilon
+print(abs(3.0 * (4.0 / 3.0 - 1) - 1)+machineEpsilon())
