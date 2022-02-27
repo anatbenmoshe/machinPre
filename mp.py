e4519b06d8ae43746223dbcaffa0ccf969aa4a3a
anatbenmoshe <62757587+anatbenmoshe@users.noreply.github.com>
2022-02-27 10:16:57 +0200

    commit4

--- mp.py
+++ mp.py
@@ -12,7 +12,3 @@ def machineEpsilon():  # find and return machine epsilon
 
 print(machineEpsilon())  # print machine epsilon
 print(abs(3.0 * (4.0 / 3.0 - 1) - 1)+machineEpsilon())
-print("aaaaa")
-print("commit2")
-print("commit3")
-print("pull")
